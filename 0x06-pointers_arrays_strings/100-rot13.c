b320f6aeb8664fdd3f1767b2837ba939ecb98155
Sami4code <hprsamuel@gmail.com>
2023-03-02 19:45:46 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: parameter pointer
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+int i;
+int j;
+
+char data 1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == data 1[j])
+{
+s[i] = datarot[j];
+break;
+}
+}
+}
+return (s);
+}
+
