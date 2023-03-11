51f3a19c40c5a711c0540e7bb15ed300d3019faf
Sami4code <hprsamuel@gmail.com>
2023-03-10 18:39:09 -0800

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -9,12 +9,6 @@
 
 void _puts(char *s)
 {
-	int i = 0;
-
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
