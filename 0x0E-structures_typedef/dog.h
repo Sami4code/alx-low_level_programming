93e1504cf3b6c3a3729509064dbf1ac68bb60825
Sami4code <hprsamuel@gmail.com>
2023-03-21 05:49:26 -0700

    function that creates a new dog.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,29 +2,25 @@
 #define DOG_H
 
 /**
- * struct dog - defines dog
- * @name: name parameter
+ * struct dog - structure definition of a dog
+ * @name: character string
  * @age: integer
- * @owner: owner parameter
+ * @owner: character string
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char src);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 
 #endif
